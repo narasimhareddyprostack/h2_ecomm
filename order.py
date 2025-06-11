bd26203cf72b59539e30b01dccc27399742588db
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-06-11 10:47:48 +0530

    tst

---
+++ order.py
@@ -0,0 +1,13 @@
+def my_decorator(func):
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+# Example usage
+say_hello()
